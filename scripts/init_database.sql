fb808f99c8e29f33294afe3c8d90ed89d0744f78
Md SaDHiN <sksadhinboss@gmail.com>
2026-01-13 17:08:19 +0600

    first change

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+================================================================================
+  CREATE DATABASE AND SCEMAS
+================================================================================
+*/
+
+use master;
+GO  
+create database PracticeDataWareHouse;
+GO
+USE PracticeDataWareHouse
+GO
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+GO
